dea3019b2724c456bd53c9d54140a52812a155eb
amalkrishna <amalkrishna263@outlook.com>
2022-07-03 14:41:51 +0530

    Add CommandCode to represent editor commands
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

---
+++ src/main/java/com/amalstack/notebooksfx/editor/command/CommandCode.java
@@ -0,0 +1,18 @@
+package com.amalstack.notebooksfx.editor.command;
+
+public enum CommandCode {
+    BOLD,
+    EMPHASIZE,
+    UNDERLINE,
+    SUPERSCRIPT,
+    SUBSCRIPT,
+    STRIKETHROUGH,
+    CODE,
+    HEADING,
+    HORIZONTAL_RULE,
+    ORDERED_LIST,
+    UNORDERED_LIST,
+    URL,
+    IMAGE,
+    TABLE
+}
