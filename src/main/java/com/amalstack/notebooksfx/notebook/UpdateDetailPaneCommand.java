f1bc2dc34292e6a5481a4c8d610338d307ecc74c
amalkrishna <amalkrishna263@outlook.com>
2022-07-06 21:27:00 +0530

    Add UpdateDetailPaneCommand
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

---
+++ src/main/java/com/amalstack/notebooksfx/notebook/UpdateDetailPaneCommand.java
@@ -0,0 +1,31 @@
+package com.amalstack.notebooksfx.notebook;
+
+import com.amalstack.notebooksfx.Command;
+import javafx.scene.control.Label;
+import org.controlsfx.control.MasterDetailPane;
+
+public class UpdateDetailPaneCommand implements Command<NotebookViewModel, Void> {
+    private final Context context;
+
+    public UpdateDetailPaneCommand(Context context) {
+        this.context = context;
+    }
+
+    public Void execute(NotebookViewModel model) {
+        if (model == null) {
+            context.masterDetailPane().setShowDetailNode(false);
+            return null;
+        }
+        context.notebookTitleLabel().setText(model.getName());
+        context.notebookDescLabel().setText(model.getUsername());
+        context.masterDetailPane().setShowDetailNode(true);
+        return null;
+    }
+
+    public record Context(
+            MasterDetailPane masterDetailPane,
+            Label notebookTitleLabel,
+            Label notebookDescLabel
+    ) {
+    }
+}
