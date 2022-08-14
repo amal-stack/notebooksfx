b970b26bd22000afcdd9e089d9f179b94eb6b74b
amalkrishna <amalkrishna263@outlook.com>
2022-08-14 16:54:04 +0530

    Add utility methods to service container
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/di/Container.java
+++ src/main/java/com/amalstack/notebooksfx/di/Container.java
@@ -6,10 +6,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public final class Container {
+
     private final Map<Class<?>, ServiceDescriptor<?, ?>> services = new HashMap<>();
     private final Map<Class<?>, Object> singletonInstances = new HashMap<>();
 
-    public <T> void addService(Class<T> abstraction, Class<? extends T> implementation, Lifetime lifetime) {
+
+    public <T> void addService(Class<T> abstraction,
+                               Class<? extends T> implementation,
+                               Lifetime lifetime) {
         validateAbstraction(abstraction);
         validateService(implementation);
         services.put(abstraction, new ServiceDescriptor<>(abstraction, implementation, lifetime));
@@ -17,7 +21,7 @@ public final class Container {
 
     @SuppressWarnings("unchecked")
     public <T> T getService(Class<T> abstraction) {
-        var descriptor = (ServiceDescriptor<T, ? extends T>)services.get(abstraction);
+        var descriptor = (ServiceDescriptor<T, ? extends T>) services.get(abstraction);
         if (descriptor == null) {
             throw new ServiceNotFoundException(abstraction);
         }
@@ -25,7 +29,7 @@ public final class Container {
 
         Constructor<? extends T> publicConstructor = (Constructor<? extends T>) implType.getConstructors()[0];
 
-        return switch(descriptor.lifetime()) {
+        return switch (descriptor.lifetime()) {
             case TRANSIENT -> newInstanceOf(publicConstructor);
             case SINGLETON -> {
                 T instance = (T) singletonInstances.getOrDefault(abstraction, null);
@@ -38,12 +42,32 @@ public final class Container {
         };
     }
 
+
     @SuppressWarnings("unchecked")
     public <T> T injectAndConstruct(Class<T> clazz) {
         validateService(clazz);
         Constructor<? extends T> publicConstructor = (Constructor<? extends T>) clazz.getConstructors()[0];
         return newInstanceOf(publicConstructor);
     }
+
+    public Lifetime getServiceLifetime(Class<?> abstraction) {
+        var descriptor = services.get(abstraction);
+        if (descriptor == null) {
+            throw new ServiceNotFoundException(abstraction);
+        }
+        return descriptor.lifetime();
+    }
+
+    public <T> void addSingleton(Class<T> abstraction,
+                                 Class<? extends T> implementation) {
+        addService(abstraction, implementation, Lifetime.SINGLETON);
+    }
+
+    public <T> void addTransient(Class<T> abstraction,
+                                 Class<? extends T> implementation) {
+        addService(abstraction, implementation, Lifetime.TRANSIENT);
+    }
+
 
     private <T> T newInstanceOf(Constructor<T> constructor) {
         var parameterTypes = constructor.getParameterTypes();
@@ -62,13 +86,13 @@ public final class Container {
 
     private void validateService(Class<?> implementation) {
         if (implementation.getConstructors().length != 1) {
-            throw new IllegalArgumentException("The service implementation must have exactly one public constructor");
+            throw new IllegalArgumentException("The service implementation " + implementation.getName() + " must have exactly one public constructor");
         }
     }
 
     private void validateAbstraction(Class<?> abstraction) {
         if (!Modifier.isAbstract(abstraction.getModifiers()) && !abstraction.isInterface()) {
-            throw new IllegalArgumentException("The parameter \"abstraction\" must represent an abstract class or an interface");
+            throw new IllegalArgumentException("The abstraction " + abstraction.getName() + "  must represent an abstract class or an interface");
         }
     }
 }
