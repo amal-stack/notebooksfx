dc06c5b18a4b267aae333844c783c7b7c00b9e8e
amalkrishna <amalkrishna263@outlook.com>
2023-02-12 19:38:50 +0530

    Remove HttpResult.Builder and add factory methods instead for construction.
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/util/http/HttpResult.java
+++ src/main/java/com/amalstack/notebooksfx/util/http/HttpResult.java
@@ -50,40 +50,4 @@ public class HttpResult<T, E extends ErrorResponse> {
     public static <T> HttpResult<T, ? extends ErrorResponse> ofError(HttpResponse<String> response, ErrorResponse error) {
         return new HttpResult<>(response, null, error);
     }
-
-    public static <T, E extends ErrorResponse> Builder<T, E> builder(Class<T> type, Class<E> errorType) {
-        return new Builder<>();
-    }
-
-    public static <T, E extends ErrorResponse> Builder<T, E> builder() {
-        return new Builder<>();
-    }
-
-    public static class Builder<T, E extends ErrorResponse> {
-        private HttpResponse<String> response;
-        private T object;
-        private E error;
-
-        private Builder() {
-        }
-
-        public Builder<T, E> withResponse(HttpResponse<String> response) {
-            this.response = response;
-            return this;
-        }
-
-        public Builder<T, E> withObject(T object) {
-            this.object = object;
-            return this;
-        }
-
-        public Builder<T, E> withError(E error) {
-            this.error = error;
-            return this;
-        }
-
-        public HttpResult<T, E> build() {
-            return new HttpResult<>(response, object, error);
-        }
-    }
 }
