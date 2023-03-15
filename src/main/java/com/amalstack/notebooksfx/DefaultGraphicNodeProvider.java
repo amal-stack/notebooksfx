4dba846fbd2cdd870e68d6370490fafb57f1abe0
amalkrishna <amalkrishna263@outlook.com>
2023-03-15 19:22:43 +0530

    Add constant for "FontAwesome"
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/DefaultGraphicNodeProvider.java
+++ src/main/java/com/amalstack/notebooksfx/DefaultGraphicNodeProvider.java
@@ -6,15 +6,19 @@ import org.controlsfx.glyphfont.FontAwesome;
 import org.controlsfx.glyphfont.Glyph;
 
 public class DefaultGraphicNodeProvider implements GraphicNodeProvider {
+
+    public static final String FONT_FAMILY = "FontAwesome";
+
+    @Override
     public Node getNode(Graphic graphic) {
         return switch (graphic) {
-            case NOTEBOOK -> new Glyph("FontAwesome", FontAwesome.Glyph.BOOK);
-            case SECTION -> new Glyph("FontAwesome", FontAwesome.Glyph.COLUMNS);
-            case PAGE -> new Glyph("FontAwesome", FontAwesome.Glyph.FILE_ALT);
-            case REFRESH -> new Glyph("FontAwesome", FontAwesome.Glyph.REFRESH);
-            case SHOW -> new Glyph("FontAwesome", FontAwesome.Glyph.EYE);
-            case HIDE -> new Glyph("FontAwesome", FontAwesome.Glyph.EYE_SLASH);
-            case ERROR -> new Glyph("FontAwesome", FontAwesome.Glyph.EXCLAMATION_TRIANGLE).color(Color.CRIMSON);
+            case NOTEBOOK -> new Glyph(FONT_FAMILY, FontAwesome.Glyph.BOOK);
+            case SECTION -> new Glyph(FONT_FAMILY, FontAwesome.Glyph.COLUMNS);
+            case PAGE -> new Glyph(FONT_FAMILY, FontAwesome.Glyph.FILE_ALT);
+            case REFRESH -> new Glyph(FONT_FAMILY, FontAwesome.Glyph.REFRESH);
+            case SHOW -> new Glyph(FONT_FAMILY, FontAwesome.Glyph.EYE);
+            case HIDE -> new Glyph(FONT_FAMILY, FontAwesome.Glyph.EYE_SLASH);
+            case ERROR -> new Glyph(FONT_FAMILY, FontAwesome.Glyph.EXCLAMATION_TRIANGLE).color(Color.CRIMSON);
         };
     }
 }
