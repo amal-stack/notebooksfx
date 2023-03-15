f55cb33a568b4207b9493547d2b0d2a126e17e3e
amalkrishna <amalkrishna263@outlook.com>
2023-03-15 19:36:10 +0530

    Update Command references and implementations to use the new interfaces
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/notebook/UpdateDetailPaneCommand.java
+++ src/main/java/com/amalstack/notebooksfx/notebook/UpdateDetailPaneCommand.java
@@ -1,26 +1,25 @@
 package com.amalstack.notebooksfx.notebook;
 
-import com.amalstack.notebooksfx.Command;
+import com.amalstack.notebooksfx.command.ParameterizedCommand;
 import javafx.scene.control.Label;
 import org.controlsfx.control.MasterDetailPane;
 
-public class UpdateDetailPaneCommand implements Command<NotebookViewModel, Void> {
+public class UpdateDetailPaneCommand implements ParameterizedCommand<NotebookViewModel> {
     private final Context context;
 
     public UpdateDetailPaneCommand(Context context) {
         this.context = context;
     }
 
-    public Void execute(NotebookViewModel model) {
+    public void execute(NotebookViewModel model) {
         if (model == null) {
             context.masterDetailPane().setShowDetailNode(false);
-            return null;
+            return;
         }
 
         context.notebookTitleLabel().setText(model.getName());
         context.notebookDescLabel().setText(model.getDescription());
         context.masterDetailPane().setShowDetailNode(true);
-        return null;
     }
 
     public record Context(
