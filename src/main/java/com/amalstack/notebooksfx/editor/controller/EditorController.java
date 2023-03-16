e916baeb476b0fac0a7001dbfef038a8fb02f181
amalkrishna <amalkrishna263@outlook.com>
2023-03-16 15:59:35 +0530

    Use new commands and initializers
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/editor/controller/EditorController.java
+++ src/main/java/com/amalstack/notebooksfx/editor/controller/EditorController.java
@@ -2,13 +2,16 @@ package com.amalstack.notebooksfx.editor.controller;
 
 import com.amalstack.notebooksfx.Graphic;
 import com.amalstack.notebooksfx.GraphicNodeProvider;
+import com.amalstack.notebooksfx.command.CommandExecutor;
+import com.amalstack.notebooksfx.controller.PageTreeItemModel;
+import com.amalstack.notebooksfx.data.DataAccessService;
 import com.amalstack.notebooksfx.editor.EditorContext;
 import com.amalstack.notebooksfx.editor.EditorContextFactory;
-import javafx.beans.binding.Bindings;
-import javafx.event.ActionEvent;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
@@ -19,6 +22,7 @@ import org.fxmisc.richtext.StyleClassedTextArea;
 public class EditorController {
     private final EditorContextFactory factory;
     private final GraphicNodeProvider graphic;
+    private final DataAccessService dataAccessService;
 
     private EditorContext context;
 
@@ -29,7 +33,7 @@ public class EditorController {
     @FXML
     private ToolBar mainToolbar;
     @FXML
-    private Button viewSectionsBtn;
+    private ToggleButton viewSectionsBtn;
     @FXML
     private Button newSectionBtn;
     @FXML
@@ -39,7 +43,7 @@ public class EditorController {
     @FXML
     private StyleClassedTextArea editorTextArea;
     @FXML
-    private Button saveBtn;
+    private Button refreshBtn;
     @FXML
     private WebView outputWebView;
     @FXML
@@ -53,82 +57,57 @@ public class EditorController {
     private NotebookTreeViewController notebookTreeViewController;
 
     public EditorController(EditorContextFactory factory,
-                            GraphicNodeProvider graphic) {
+                            GraphicNodeProvider graphic,
+                            DataAccessService dataAccessService) {
         this.factory = factory;
         this.graphic = graphic;
+        this.dataAccessService = dataAccessService;
     }
 
     public void initialize() {
         context = factory.create(editorTextArea);
-        addEditorToolbarControls();
-        initOutputWebView();
+
+        Initializer.initialize(
+                new EditorToolbarInitializer(context, editorToolbar),
+                new OutputWebViewInitializer(outputWebView, context, refreshBtn, webViewProgress, graphic)
+        );
+
+        viewSectionsBtn.selectedProperty().bindBidirectional(masterDetailPane.showDetailNodeProperty());
+        viewSectionsBtn.selectedProperty().addListener(this::toggleGraphic);
+
         // default initial state
-        masterDetailPane.setShowDetailNode(true);
-        viewSectionsBtn.setGraphic(graphic.getNode(Graphic.SHOW));
-        viewSectionsBtn.setOnAction(this::toggleDetailPane);
-        editorTextArea.setEditable(false);
-        editorTextArea.replaceText("Select a page to edit");
+        //masterDetailPane.setShowDetailNode(true);
+        CommandExecutor.execute(new ToggleNotebookTreeViewCommand(masterDetailPane, viewSectionsBtn, graphic), true);
+        CommandExecutor.execute(new DisableEditorCommand(editorTextArea));
 
-        notebookTreeViewController.currentPageProperty().addListener((observable, previousPage, newPage) -> {
-            if (previousPage != null) {
-
-                //TODO: Save contents of previous page
-            }
-            if (newPage != null) {
-                editorTextArea.setEditable(true);
-                editorTextArea.replaceText(newPage.getContent());
-                previewHtml();
-                return;
-            }
-            editorTextArea.setEditable(false);
-            editorTextArea.replaceText("Select a page to edit");
-        });
+        notebookTreeViewController.currentPageProperty().addListener(this::onPageChanged);
     }
 
-    private void addEditorToolbarControls() {
-        new EditorToolbarInitializer(context, editorToolbar).addControls();
-    }
-
-    private void initOutputWebView() {
-        saveBtn.setOnAction(event -> previewHtml());
-        saveBtn.setGraphic(graphic.getNode(Graphic.REFRESH));
-
-        var progressProperty = webViewProgress.progressProperty();
-
-        // Bind progress to web engine load worker's progress
-        progressProperty.bind(outputWebView
-                .getEngine()
-                .getLoadWorker()
-                .progressProperty());
-
-        // Show progress only if value is greater than 0 and less than 1
-        webViewProgress.visibleProperty()
-                .bind(Bindings
-                        .when(progressProperty.lessThan(0)
-                                .or(progressProperty.isEqualTo(1)))
-                        .then(false)
-                        .otherwise(true));
-    }
-
-
-    private void previewHtml() {
-        String html = context.toHtml();
-        //System.out.println(html);
-        outputWebView.getEngine().loadContent(html, "text/html");
-    }
-
-    private void toggleDetailPane(ActionEvent event) {
-        boolean shouldShow = !masterDetailPane.isShowDetailNode();
-        if (shouldShow) {
-            // show
-            masterDetailPane.setShowDetailNode(true);
+    private void toggleGraphic(ObservableValue<? extends Boolean> observable,
+                               Boolean oldValue,
+                               Boolean newValue) {
+        if (newValue) {
             viewSectionsBtn.setGraphic(graphic.getNode(Graphic.HIDE));
             return;
         }
-        // hide
-        masterDetailPane.setShowDetailNode(false);
         viewSectionsBtn.setGraphic(graphic.getNode(Graphic.SHOW));
     }
 
+    private void onPageChanged(ObservableValue<? extends PageTreeItemModel> observable,
+                               PageTreeItemModel previousPage,
+                               PageTreeItemModel currentPage) {
+        if (previousPage != null) {
+            //TODO: Save contents of previous page
+
+        }
+        if (currentPage != null) {
+            editorTextArea.setEditable(true);
+            editorTextArea.replaceText(currentPage.getContent());
+            CommandExecutor.execute(new PreviewHtmlCommand(context, outputWebView));
+            return;
+        }
+        CommandExecutor.execute(new DisableEditorCommand(editorTextArea));
+    }
 }
+
 
