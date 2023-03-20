0e572c9f4de6393fb8cee1a92683d33d4221e182
amalkrishna <amalkrishna263@outlook.com>
2023-03-20 17:55:58 +0530

    Allow deleting notebooks
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

---
+++ src/main/java/com/amalstack/notebooksfx/controller/DeleteNotebookCommand.java
@@ -0,0 +1,34 @@
+package com.amalstack.notebooksfx.controller;
+
+import com.amalstack.notebooksfx.command.Command;
+import com.amalstack.notebooksfx.data.DataAccessService;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+class DeleteNotebookCommand implements Command {
+    private final Long notebookId;
+
+    private final String notebookName;
+
+    private final DataAccessService dataAccessService;
+
+    public DeleteNotebookCommand(Long notebookId, String notebookName, DataAccessService dataAccessService) {
+        this.notebookId = notebookId;
+        this.notebookName = notebookName;
+        this.dataAccessService = dataAccessService;
+    }
+
+    @Override
+    public void execute() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                "Are you sure you want to delete the notebook " + notebookName + "?",
+                ButtonType.YES,
+                ButtonType.NO);
+        alert.showAndWait().ifPresent(buttonType -> {
+            if (buttonType == ButtonType.YES) {
+                dataAccessService.notebooks().delete(notebookId);
+            }
+        });
+
+    }
+}
