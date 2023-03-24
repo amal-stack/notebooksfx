aa34a45974e20d0206ff9bb95dd7111ab416648f
amalkrishna <amalkrishna263@outlook.com>
2023-03-24 19:11:07 +0530

    Skip editor command execution if editor is disabled
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/editor/command/text/TextEditorCommand.java
+++ src/main/java/com/amalstack/notebooksfx/editor/command/text/TextEditorCommand.java
@@ -1,7 +1,27 @@
 package com.amalstack.notebooksfx.editor.command.text;
 
 import com.amalstack.notebooksfx.command.ParameterizedResultCommand;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import org.fxmisc.richtext.StyleClassedTextArea;
+
+import java.util.ResourceBundle;
 
 @FunctionalInterface
 public interface TextEditorCommand extends ParameterizedResultCommand<CharSequence, CharSequence> {
+    static EventHandler<ActionEvent> handleEditorCommand(StyleClassedTextArea textArea,
+                                                         TextEditorCommand command,
+                                                         ResourceBundle resources) {
+        return event -> {
+            if (textArea.isDisabled()) {
+                return;
+            }
+            var selection = textArea.getSelection();
+            if (selection.getLength() == 0) {
+                textArea.appendText(command.execute(resources.getString("editor.command.text.placeholder")).toString());
+                return;
+            }
+            textArea.replaceText(selection, command.execute(textArea.getText(selection)).toString());
+        };
+    }
 }
