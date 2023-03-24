df5c590bf77983cde524d032791b9ea2cbcd0b78
amalkrishna <amalkrishna263@outlook.com>
2023-03-24 19:21:20 +0530

    Scaffold `AboutCommand`
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

---
+++ src/main/java/com/amalstack/notebooksfx/editor/command/AboutCommand.java
@@ -0,0 +1,14 @@
+package com.amalstack.notebooksfx.editor.command;
+
+import com.amalstack.notebooksfx.command.Command;
+import javafx.stage.Stage;
+
+public class AboutCommand implements Command {
+    public AboutCommand(Stage stage) {
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
