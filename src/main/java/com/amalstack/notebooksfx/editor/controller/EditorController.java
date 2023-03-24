50d931870e8e8a11ed7d4ac4deaefc13d85c6996
amalkrishna <amalkrishna263@outlook.com>
2023-03-24 19:30:30 +0530

    Set on `TreeView` context created. Initialize menu bar.
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/editor/controller/EditorController.java
+++ src/main/java/com/amalstack/notebooksfx/editor/controller/EditorController.java
@@ -6,6 +6,7 @@ import com.amalstack.notebooksfx.data.DataAccessService;
 import com.amalstack.notebooksfx.editor.EditorContext;
 import com.amalstack.notebooksfx.editor.EditorContextFactory;
 import com.amalstack.notebooksfx.editor.command.*;
+import com.amalstack.notebooksfx.editor.nav.NotebookTreeViewContext;
 import com.amalstack.notebooksfx.editor.nav.NotebookTreeViewController;
 import com.amalstack.notebooksfx.editor.nav.PageTreeItemModel;
 import com.amalstack.notebooksfx.nav.NavigationManager;
@@ -14,15 +15,14 @@ import com.amalstack.notebooksfx.util.controls.Graphic;
 import com.amalstack.notebooksfx.util.controls.GraphicNodeProvider;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import org.controlsfx.control.MasterDetailPane;
 import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.fxmisc.richtext.StyleClassedTextArea;
+
+import java.util.ResourceBundle;
 
 public class EditorController {
     private final EditorContextFactory factory;
@@ -31,6 +31,9 @@ public class EditorController {
     private final NavigationManager navigationManager;
 
     private EditorContext context;
+
+    @FXML
+    private MenuBar editorMenuBar;
 
     @FXML
     private VirtualizedScrollPane<StyleClassedTextArea> editorScrollPane;
@@ -60,6 +63,9 @@ public class EditorController {
     @FXML
     private NotebookTreeViewController notebookTreeViewController;
 
+    @FXML
+    private ResourceBundle resources;
+
     public EditorController(EditorContextFactory factory,
                             GraphicNodeProvider graphic,
                             DataAccessService dataAccessService,
@@ -71,29 +77,33 @@ public class EditorController {
     }
 
     public void initialize() {
-        context = factory.create(editorTextArea);
+        context = factory.create(editorTextArea, outputWebView);
 
         Initializer.initialize(
                 new EditorToolbarInitializer(context, editorToolbar),
-                new OutputWebViewInitializer(outputWebView, context, refreshBtn, webViewProgress, graphic)
+                new OutputWebViewInitializer(context, refreshBtn, webViewProgress, graphic, resources)
         );
 
+
+        viewSectionsBtn.setGraphic(graphic.getNode(Graphic.SECTION));
         viewSectionsBtn.selectedProperty().bindBidirectional(masterDetailPane.showDetailNodeProperty());
         viewSectionsBtn.selectedProperty().addListener(this::toggleGraphic);
 
         backToNotebooksBtn.setGraphic(graphic.getNode(Graphic.BACK));
         backToNotebooksBtn.setOnAction(event -> {
-            savePage(notebookTreeViewController.getCurrentPage());
+            savePage(notebookTreeViewController.getContext().getCurrentPage());
             navigationManager.navigateTo(Parents.HOME);
         });
+
+        notebookTreeViewController.setOnContextCreated(this::onNotebookTreeViewContextCreated);
 
         // default initial state
         //masterDetailPane.setShowDetailNode(true);
         CommandExecutor.execute(new ToggleNotebookTreeViewCommand(masterDetailPane, viewSectionsBtn, graphic), true);
         CommandExecutor.execute(new DisableEditorCommand(editorTextArea));
 
-        notebookTreeViewController.currentPageProperty().addListener(this::onPageChanged);
     }
+
 
     private void toggleGraphic(ObservableValue<? extends Boolean> observable,
                                Boolean oldValue,
@@ -112,17 +122,35 @@ public class EditorController {
             savePage(previousPage);
         }
         if (currentPage != null) {
-            CommandExecutor.execute(
-                    new LoadPageIntoEditorCommand(editorTextArea, currentPage),
-                    new PreviewHtmlCommand(context, outputWebView)
-            );
+            loadPage(currentPage);
             return;
         }
         CommandExecutor.execute(new DisableEditorCommand(editorTextArea));
     }
 
+    private void loadPage(PageTreeItemModel currentPage) {
+        CommandExecutor.execute(
+                new LoadPageIntoEditorCommand(editorTextArea, currentPage),
+                new PreviewHtmlCommand(context)
+        );
+    }
+
     private void savePage(PageTreeItemModel previousPage) {
         CommandExecutor.execute(new SavePageCommand(dataAccessService, context), previousPage);
+    }
+
+    private void onNotebookTreeViewContextCreated(NotebookTreeViewContext notebookTreeViewContext) {
+        Initializer.initialize(new EditorMenuBarInitializer(editorMenuBar,
+                resources,
+                notebookTreeViewContext,
+                context,
+                navigationManager.getStage()));
+
+        var currentPage = notebookTreeViewContext.getCurrentPage();
+        if (currentPage != null) {
+            loadPage(currentPage);
+        }
+        notebookTreeViewContext.currentPageProperty().addListener(this::onPageChanged);
     }
 }
 
