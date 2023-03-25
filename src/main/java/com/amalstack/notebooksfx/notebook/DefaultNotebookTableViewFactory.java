ca784ee5c90206de96dbeb8a0f3cde2e6ede10c6
amalkrishna <amalkrishna263@outlook.com>
2023-03-25 12:49:25 +0530

    Use ResourceBundle and GraphicNodeProvider in `NotebookTableViewFactory`
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/notebook/DefaultNotebookTableViewFactory.java
+++ src/main/java/com/amalstack/notebooksfx/notebook/DefaultNotebookTableViewFactory.java
@@ -1,6 +1,8 @@
 package com.amalstack.notebooksfx.notebook;
 
 import com.amalstack.notebooksfx.command.ParameterizedCommand;
+import com.amalstack.notebooksfx.util.controls.Graphic;
+import com.amalstack.notebooksfx.util.controls.GraphicNodeProvider;
 import com.amalstack.notebooksfx.util.controls.builder.TableColumnConfig;
 import com.amalstack.notebooksfx.util.controls.builder.TableViewBuilder;
 import javafx.collections.ObservableList;
@@ -9,10 +11,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import org.controlsfx.glyphfont.FontAwesome;
-import org.controlsfx.glyphfont.Glyph;
 
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.function.BiPredicate;
 
 public class DefaultNotebookTableViewFactory implements NotebookTableViewFactory {
@@ -21,28 +22,44 @@ public class DefaultNotebookTableViewFactory implements NotebookTableViewFactory
             .toLowerCase()
             .contains(query);
 
-    private static List<TableColumnConfig<NotebookViewModel, ?>> getColumns() {
+    private static List<TableColumnConfig<NotebookViewModel, ?>> getColumns(ResourceBundle resources, GraphicNodeProvider graphic) {
         return List.of(
-                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>("Notebook Name"), model -> model.getValue().nameProperty(), new Glyph("FontAwesome", FontAwesome.Glyph.BOOK)),
-                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>("Username"), model -> model.getValue().usernameProperty(), new Glyph("FontAwesome", FontAwesome.Glyph.USER)),
-                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>("Created"), model -> model.getValue().creationTimeProperty(), new Glyph("FontAwesome", FontAwesome.Glyph.CLOCK_ALT)),
-                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>("Sections"), model -> model.getValue().sectionCountProperty().asString(), new Glyph("FontAwesome", FontAwesome.Glyph.SITEMAP)),
-                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>("Pages"), model -> model.getValue().pageCountProperty().asString(), new Glyph("FontAwesome", FontAwesome.Glyph.FILE))
+                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>(resources.getString("notebooks.list.table.column.name")),
+                        model -> model.getValue().nameProperty(),
+                        graphic.getNode(Graphic.NOTEBOOK)),
+
+                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>(resources.getString("notebooks.list.table.column.username")),
+                        model -> model.getValue().usernameProperty(),
+                        graphic.getNode(Graphic.USER)),
+
+                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>(resources.getString("notebooks.list.table.column.created")),
+                        model -> model.getValue().creationTimeProperty(),
+                        graphic.getNode(Graphic.CLOCK)),
+
+                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>(resources.getString("notebooks.list.table.column.sections")),
+                        model -> model.getValue().sectionCountProperty().asString(),
+                        graphic.getNode(Graphic.SECTION)),
+
+                new TableColumnConfig<NotebookViewModel, String>(new TableColumn<>(resources.getString("notebooks.list.table.column.pages")),
+                        model -> model.getValue().pageCountProperty().asString(),
+                        graphic.getNode(Graphic.PAGE))
         );
     }
 
-    private static Node getPlaceHolder() {
-        return new Label("No notebooks found");
+    private static Node getPlaceHolder(ResourceBundle resources) {
+        return new Label(resources.getString("notebooks.list.empty.prompt"));
     }
 
     @Override
     public TableView<NotebookViewModel> create(
             ObservableList<NotebookViewModel> data,
             TextField searchTextField,
-            ParameterizedCommand<NotebookViewModel> rowClickCommand) {
+            ParameterizedCommand<NotebookViewModel> rowClickCommand,
+            ResourceBundle resources,
+            GraphicNodeProvider graphicNodeProvider) {
         return new TableViewBuilder<NotebookViewModel>()
-                .withId("notebooksTableView").hasPlaceholder(getPlaceHolder())
-                .addColumns(getColumns()).withData(data)
+                .withId("notebooksTableView").hasPlaceholder(getPlaceHolder(resources))
+                .addColumns(getColumns(resources, graphicNodeProvider)).withData(data)
                 .onRowClick(rowClickCommand)
                 .filter(searchTextField.textProperty(), SEARCH_PREDICATE)
                 .sort()
--- src/main/java/com/amalstack/notebooksfx/notebook/NotebookTableViewFactory.java
+++ src/main/java/com/amalstack/notebooksfx/notebook/NotebookTableViewFactory.java
@@ -1,13 +1,18 @@
 package com.amalstack.notebooksfx.notebook;
 
 import com.amalstack.notebooksfx.command.ParameterizedCommand;
+import com.amalstack.notebooksfx.util.controls.GraphicNodeProvider;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import java.util.ResourceBundle;
 
 public interface NotebookTableViewFactory {
     TableView<NotebookViewModel> create(
             ObservableList<NotebookViewModel> data,
             TextField searchTextField,
-            ParameterizedCommand<NotebookViewModel> rowClickCommand);
+            ParameterizedCommand<NotebookViewModel> rowClickCommand,
+            ResourceBundle resources,
+            GraphicNodeProvider graphicNodeProvider);
 }
