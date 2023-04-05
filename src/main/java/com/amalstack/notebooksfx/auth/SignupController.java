a474339d996b68ab3b267329cc6e11d934c08b0c
amalkrishna <amalkrishna263@outlook.com>
2023-04-05 14:41:12 +0530

    Use Alert to display signup errors and information, add onSignUpCommand
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/auth/SignupController.java
+++ src/main/java/com/amalstack/notebooksfx/auth/SignupController.java
@@ -1,24 +1,23 @@
 package com.amalstack.notebooksfx.auth;
 
-import com.amalstack.notebooksfx.data.model.ErrorEntry;
+import com.amalstack.notebooksfx.command.Command;
+import com.amalstack.notebooksfx.command.CommandExecutor;
+import com.amalstack.notebooksfx.css.StylesheetLocator;
+import com.amalstack.notebooksfx.css.Stylesheets;
 import com.amalstack.notebooksfx.data.model.User;
 import com.amalstack.notebooksfx.data.model.UserRegistration;
+import com.amalstack.notebooksfx.util.controls.Alerts;
 import com.amalstack.notebooksfx.util.http.AuthenticationService;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+
+import java.util.ResourceBundle;
 
 public class SignupController {
     private final AuthenticationService authenticationService;
-
-    private final ObservableList<ErrorEntry> signUpErrorEntries = FXCollections.observableArrayList();
-
-    @FXML
-    private ScrollPane signUpErrorScrollPane;
 
     @FXML
     private TextField nameField;
@@ -35,37 +34,64 @@ public class SignupController {
     @FXML
     private Button signUpButton;
 
+    @FXML
+    private ResourceBundle resources;
+
+    private Command onSignUpSuccess;
+
     public SignupController(AuthenticationService authenticationService) {
         this.authenticationService = authenticationService;
     }
 
     public void initialize() {
-        //errorMessageLabel.setWrapText(true);
         signUpButton.setOnAction(event -> signUp());
-        signUpErrorScrollPane.setContent(ErrorTableViewFactory.create(signUpErrorEntries, "signUpView"));
     }
 
     public void signUp() {
-        // Hide and clear existing errors
-        signUpErrorScrollPane.setVisible(false);
-        signUpErrorEntries.clear();
-
-
         String name = nameField.getText();
         String email = emailField.getText();
         String password = passwordField.getText();
         String confirmPassword = confirmPasswordField.getText();
 
         var result = authenticationService
-                .registerUser(new UserRegistration(email, name, password, confirmPassword), User.class);
+                .registerUser(new UserRegistration(email,
+                        name,
+                        password,
+                        confirmPassword), User.class);
 
         if (result.isSuccess()) {
+            Alerts.builder()
+                    .type(Alert.AlertType.INFORMATION)
+                    .title(resources.getString("auth.signup.success.message.title"))
+                    .header(resources.getString("auth.signup.success.message"))
+                    .stylesheets(StylesheetLocator.getStylesheet(Stylesheets.ALERT))
+                    .build()
+                    .showAndWait();
+            clearFields();
+            CommandExecutor.execute(onSignUpSuccess);
             return;
         }
 
-        result.getError().ifPresent(error -> {
-            signUpErrorEntries.addAll(ErrorEntry.fromError(error));
-            signUpErrorScrollPane.setVisible(true);
-        });
+        result.getError().ifPresent(error -> Alerts.builder()
+                .type(Alert.AlertType.ERROR)
+                .title(resources.getString("auth.signup.error.message.title"))
+                .header(resources.getString("auth.signup.error.message"))
+                .message(AuthenticationErrorResponse.getErrorMessage(error, resources))
+                .expandableContent(AuthenticationErrorResponse.createErrorNode(error, resources))
+                .stylesheets(StylesheetLocator.getStylesheet(Stylesheets.ALERT))
+                .build()
+                .showAndWait());
+    }
+
+    public void setOnSignUpSuccess(Command onSignUpSuccess) {
+        this.onSignUpSuccess = onSignUpSuccess;
+    }
+
+    private void clearFields() {
+        nameField.clear();
+        emailField.clear();
+        passwordField.clear();
+        confirmPasswordField.clear();
     }
 }
+
