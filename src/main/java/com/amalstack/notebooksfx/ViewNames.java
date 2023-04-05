f1681c63f5514545406fe6a3ec6357f9c4a1960f
amalkrishna <amalkrishna263@outlook.com>
2023-04-06 00:51:15 +0530

    Rename `Parents` to `ViewNames`
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/ViewNames.java
+++ src/main/java/com/amalstack/notebooksfx/ViewNames.java
@@ -1,11 +1,11 @@
 package com.amalstack.notebooksfx;
 
 
-public final class Parents {
+public final class ViewNames {
     public static final String HOME = "home";
     public static final String EDITOR = "editor";
     public static final String AUTH = "auth";
 
-    private Parents() {
+    private ViewNames() {
     }
 }
