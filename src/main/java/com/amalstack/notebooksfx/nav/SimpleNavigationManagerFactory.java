b5ed77c27fdc16816fcc39116c745bd2e20141b8
amalkrishna <amalkrishna263@outlook.com>
2023-04-05 14:28:21 +0530

    Remove unused class `SimpleNavigationManagerFactory`
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/nav/SimpleNavigationManagerFactory.java
+++
@@ -1,9 +0,0 @@
-package com.amalstack.notebooksfx.nav;
-
-import javafx.stage.Stage;
-
-public class SimpleNavigationManagerFactory {
-    public static SimpleNavigationManager create(Stage stage) {
-        return new SimpleNavigationManager(stage);
-    }
-}
