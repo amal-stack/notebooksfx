01c8995773ae34c4e3c3dfdb83bd444120c4fb12
amalkrishna <amalkrishna263@outlook.com>
2023-04-05 14:29:37 +0530

    Use FxmlLocator and StylesheetLocator and use default controller factory of navigation manager
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/NotebooksFxApplication.java
+++ src/main/java/com/amalstack/notebooksfx/NotebooksFxApplication.java
@@ -1,5 +1,7 @@
 package com.amalstack.notebooksfx;
 
+import com.amalstack.notebooksfx.css.StylesheetLocator;
+import com.amalstack.notebooksfx.css.Stylesheets;
 import com.amalstack.notebooksfx.data.DataAccessService;
 import com.amalstack.notebooksfx.data.HttpDataAccessService;
 import com.amalstack.notebooksfx.data.repository.NotebookRepository;
@@ -17,7 +19,6 @@ import com.amalstack.notebooksfx.editor.EditorContextFactory;
 import com.amalstack.notebooksfx.nav.NavigationManager;
 import com.amalstack.notebooksfx.nav.ParentParameters;
 import com.amalstack.notebooksfx.nav.SimpleNavigationManager;
-import com.amalstack.notebooksfx.nav.SimpleNavigationManagerFactory;
 import com.amalstack.notebooksfx.notebook.DefaultNotebookTableViewFactory;
 import com.amalstack.notebooksfx.notebook.NotebookTableViewFactory;
 import com.amalstack.notebooksfx.util.BodyMapper;
@@ -25,22 +26,26 @@ import com.amalstack.notebooksfx.util.JacksonMapper;
 import com.amalstack.notebooksfx.util.controls.DefaultGraphicNodeProvider;
 import com.amalstack.notebooksfx.util.controls.GraphicNodeProvider;
 import com.amalstack.notebooksfx.util.http.*;
+import com.amalstack.notebooksfx.views.FxmlLocator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import localization.ResourceNames;
 
 import java.util.ResourceBundle;
+import java.util.function.Supplier;
 
 import static com.amalstack.notebooksfx.AppRouteNames.*;
 
 public class NotebooksFxApplication extends Application {
+
+    private Container container;
 
     // Main method removed for exceptions to be caught by Thread.setDefaultUncaughtExceptionHandler()
 
@@ -105,13 +110,19 @@ public class NotebooksFxApplication extends Application {
                 .configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, true);
     }
 
+    private static void configureStage(Stage stage) {
+        var scene = new Scene(new Group());
+        scene.getStylesheets().add(StylesheetLocator.getStylesheet(Stylesheets.MAIN));
+        stage.setScene(scene);
+    }
+
     @Override
     public void start(Stage stage) {
         try {
             //Thread.setDefaultUncaughtExceptionHandler(this::showError);
             System.setProperty("prism.lcdtext", "false");
-            stage.setScene(new Scene(new Group()));
-            Container container = new Container();
+            configureStage(stage);
+            container = new Container();
             initServices(container);
             initNav(container, stage);
             container.getService(NavigationManager.class)
@@ -132,17 +143,20 @@ public class NotebooksFxApplication extends Application {
         }
     }
 
-    private void showError(Thread t, Throwable e) {
-        if (Platform.isFxApplicationThread()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.initModality(Modality.WINDOW_MODAL);
-            alert.setTitle("Error");
-            alert.setHeaderText("Error");
-            alert.setContentText(e.getLocalizedMessage());
-            alert.showAndWait();
-        }
-        e.printStackTrace();
-
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        var nav = container.getService(NavigationManager.class);
+        nav.getController()
+                .filter(c -> c instanceof AutoCloseable)
+                .map(c -> (AutoCloseable) c)
+                .ifPresent(autoCloseable -> {
+                    try {
+                        autoCloseable.close();
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 
     private void initServices(Container container) {
@@ -159,30 +173,34 @@ public class NotebooksFxApplication extends Application {
     }
 
     private void initNav(Container container, Stage stage) {
-        container.addService(NavigationManager.class, SimpleNavigationManager.class, Lifetime.SINGLETON, () -> SimpleNavigationManagerFactory.create(stage));
-        var nav = container.getService(NavigationManager.class);
-        var navResources = ResourceBundle.getBundle(ResourceNames.NAV);
+        container.addService(NavigationManager.class, SimpleNavigationManager.class, Lifetime.SINGLETON, navigationManagerFactory(stage));
+    }
 
-        nav.addParent(ParentParameters.builder().name(Parents.HOME)
-                .fxmlUrl(NotebooksFxApplication.class.getResource("notebooks-view.fxml"))
-                .title(navResources.getString("nav.parents.notebooks.title"))
-                .resourceBundle(ResourceNames.NOTEBOOKS)
-                .controllerFactory(container::injectAndConstruct)
-                .build());
+    private Supplier<SimpleNavigationManager> navigationManagerFactory(Stage stage) {
+        return () -> {
+            var nav = new SimpleNavigationManager(stage, container::injectAndConstruct);
+            var navResources = ResourceBundle.getBundle(ResourceNames.NAV);
 
-        nav.addParent(ParentParameters.builder().name(Parents.EDITOR)
-                .fxmlUrl(getClass().getResource("editor-view.fxml"))
-                .title(navResources.getString("nav.parents.editor.title"))
-                .resourceBundle(ResourceNames.EDITOR)
-                .controllerFactory(container::injectAndConstruct)
-                .build());
+            nav.setDefaultControllerFactory(container::injectAndConstruct);
 
-        nav.addParent(ParentParameters.builder().name(Parents.AUTH)
-                .fxmlUrl(getClass().getResource("auth-view.fxml"))
-                .title(navResources.getString("nav.parents.auth.title"))
-                .resourceBundle(ResourceNames.AUTH)
-                .controllerFactory(container::injectAndConstruct)
-                .build());
+            nav.addParent(ParentParameters.builder().name(Parents.HOME)
+                    .fxmlUrl(FxmlLocator.getFxmlUrl("notebooks-view.fxml"))
+                    .title(navResources.getString("nav.parents.notebooks.title"))
+                    .resourceBundle(ResourceNames.NOTEBOOKS)
+                    .build());
 
+            nav.addParent(ParentParameters.builder().name(Parents.EDITOR)
+                    .fxmlUrl(FxmlLocator.getFxmlUrl("editor-view.fxml"))
+                    .title(navResources.getString("nav.parents.editor.title"))
+                    .resourceBundle(ResourceNames.EDITOR)
+                    .build());
+
+            nav.addParent(ParentParameters.builder().name(Parents.AUTH)
+                    .fxmlUrl(FxmlLocator.getFxmlUrl("auth-view.fxml"))
+                    .title(navResources.getString("nav.parents.auth.title"))
+                    .resourceBundle(ResourceNames.AUTH)
+                    .build());
+            return nav;
+        };
     }
 }
