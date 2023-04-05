b37112995de24aae561b41b9e150b240cc33b809
amalkrishna <amalkrishna263@outlook.com>
2023-04-05 10:03:18 +0530

    Add empty placeholder `DefaultHttpClientInitializer`
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

---
+++ src/main/java/com/amalstack/notebooksfx/util/http/DefaultHttpClientInitializer.java
@@ -0,0 +1,9 @@
+package com.amalstack.notebooksfx.util.http;
+
+import java.net.http.HttpClient;
+
+public class DefaultHttpClientInitializer implements HttpClientInitializer {
+    @Override
+    public void initialize(HttpClient.Builder builder) {
+    }
+}
