5263f308cf960b496d21777c330293fc1fadf896
amalkrishna <amalkrishna263@outlook.com>
2023-04-05 09:57:21 +0530

    Add interface `HttpRequestInitializer` to customize request builder and headers
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

---
+++ src/main/java/com/amalstack/notebooksfx/util/http/HttpRequestInitializer.java
@@ -0,0 +1,57 @@
+package com.amalstack.notebooksfx.util.http;
+
+import java.net.http.HttpRequest;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public interface HttpRequestInitializer {
+    default void initialize(HttpRequest.Builder builder) {
+    }
+
+    default Map<String, String> headers() {
+        return Map.of();
+    }
+
+    static HttpRequestInitializer empty() {
+        return new HttpRequestInitializer() {
+        };
+    }
+
+    static HttpRequestInitializer ofHeaders(Map<String, String> headers) {
+        return new HttpRequestInitializer() {
+            @Override
+            public Map<String, String> headers() {
+                return headers;
+            }
+        };
+    }
+
+    static HttpRequestInitializer of(Consumer<HttpRequest.Builder> requestBuilderConfig) {
+        return new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest.Builder builder) {
+                if (requestBuilderConfig != null) {
+                    requestBuilderConfig.accept(builder);
+                }
+            }
+        };
+    }
+
+    static HttpRequestInitializer of(Consumer<HttpRequest.Builder> requestBuilderConfig,
+                                     Map<String, String> headers) {
+        return new HttpRequestInitializer() {
+            @Override
+            public void initialize(HttpRequest.Builder builder) {
+                if (requestBuilderConfig != null) {
+                    requestBuilderConfig.accept(builder);
+                }
+            }
+
+            @Override
+            public Map<String, String> headers() {
+                return headers;
+            }
+        };
+    }
+}
+
