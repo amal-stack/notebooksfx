647bbf667cd478c4f6b0199e515840011edfa8b4
amalkrishna <amalkrishna263@outlook.com>
2023-04-07 14:11:49 +0530

    Add default `put()` method overload with response type
    Signed-off-by: amalkrishna <amalkrishna263@outlook.com>

--- src/main/java/com/amalstack/notebooksfx/util/http/HttpClientService.java
+++ src/main/java/com/amalstack/notebooksfx/util/http/HttpClientService.java
@@ -8,15 +8,13 @@ public interface HttpClientService {
 
     <T> HttpResult<T, ? extends ErrorResponse> send(Endpoint endpoint, String method, T object);
 
-
     <T> HttpResult<T, ? extends ErrorResponse> send(Endpoint endpoint, String method, Class<T> responseClass);
 
     <S, T> HttpResult<T, ? extends ErrorResponse> send(Endpoint endpoint, String method, S object, Class<T> responseClass);
 
-    <T> HttpResult<T, ? extends ErrorResponse> send(HttpRequest request, Class<T> responseClass);
-
     <T> HttpResult<T, ? extends ErrorResponse> send(HttpRequest request);
 
+    <T> HttpResult<T, ? extends ErrorResponse> send(HttpRequest request, Class<T> responseClass);
 
     default <T> HttpResult<T, ? extends ErrorResponse> get(Endpoint endpoint, Class<T> responseClass) {
         return send(endpoint, "GET", responseClass);
@@ -24,6 +22,10 @@ public interface HttpClientService {
 
     default <T> HttpResult<T, ? extends ErrorResponse> post(Endpoint endpoint, T object) {
         return send(endpoint, "POST", object);
+    }
+
+    default <S, T> HttpResult<T, ? extends ErrorResponse> post(Endpoint endpoint, S object, Class<T> responseClass) {
+        return send(endpoint, "POST", object, responseClass);
     }
 
     default <T> HttpResult<T, ? extends ErrorResponse> put(Endpoint endpoint, T object) {
